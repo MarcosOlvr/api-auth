6866767dbcc06fcd8c0e5ba0db50e1c825dcc34c
Marcos <marquinhooliveira9968@outlook.com>
2021-11-09 23:54:28 -0300

    first commit

---
+++ Settings.cs
@@ -0,0 +1,7 @@
+namespace ApiAuth
+{
+    public static class Settings
+    {
+        public static string Secret = "ashjudanskdhub1321jh3b1h2jb4j1k2l4kasjkdbnaskjlnd4j12jk!@!@!#!dsafjnksdfljkn@#";
+    }
+}
